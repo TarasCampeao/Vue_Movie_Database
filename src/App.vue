cd9595d786d78634a02bec1b544a3838dd5a4d3e
Taras Vitovshchyk <vitovstreet@gmail.com>
2019-01-02 22:41:09 +0200

    start files

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <movie-list/>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app',
+  components: {
+  	MovieList
+  },
+};
+</script>
+
+
---
+++ src/components/MovieList.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="container">
+		
+	</div>
+</template>
+
+
+<script>
+	
+
+export default {
+	name: 'MovieList',
+	data() {
+		return {
+
+		}
+	}
+}
+
+</script>
