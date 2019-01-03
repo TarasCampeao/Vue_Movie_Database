5390e834563da0d3cde4fabedfa03dd821ee40e4
Taras Vitovshchyk <vitovstreet@gmail.com>
2019-01-03 13:34:32 +0200

    database connection

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,24 @@
+<style lang="sass">
+    @import url('https://use.fontawesome.com/releases/v5.3.1/css/all.css')
+    @import "./assets/style/main.sass"
+    @import "./assets/style/media.sass"
+</style>
 <template>
   <div id="app">
-    <movie-list/>
+    <movie-list></movie-list>
   </div>
 </template>
 
 <script>
+import MovieList from './components/MovieList'
 
 export default {
   name: 'app',
   components: {
-  	MovieList
+  	MovieList,
   },
-};
+}
+
 </script>
 
 
--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -1,20 +1,42 @@
 <template>
 	<div class="container">
-		
+		<h1>Movie List</h1>
+		<ul>
+			<li v-for="item in items.results" :key="item.id">
+				<div class="img_size">
+					<img :src="imageUrl + item.poster_path" alt=""/>
+				</div>
+				<div class="desc_item">{{ item.overview }}</div>
+			</li>
+		</ul>
 	</div>
 </template>
 
 
 <script>
-	
 
 export default {
 	name: 'MovieList',
 	data() {
 		return {
-
+			items: [],
+			baseUrl: 'https://api.themoviedb.org/3',
+			apiKey: '1b62ccff88d2cd537027e1d82920197b',
+			imageUrl: 'https://image.tmdb.org/t/p/w342',
+			//loaded: true
 		}
-	}
+	},
+    created: function() {
+        this.fetchData();
+    },
+    methods: {
+        fetchData: function() {
+            this.$http.get(this.baseUrl + '/discover/movie?api_key=' + this.apiKey + '&sort_by=popularity.desc').then(response => {
+                this.items = response.body;
+                this.loaded = false;
+            });
+        }
+    }
 }
 
 </script>
